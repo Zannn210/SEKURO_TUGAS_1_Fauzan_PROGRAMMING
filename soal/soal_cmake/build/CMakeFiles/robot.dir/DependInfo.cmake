317cc804ace9d269cacfd88084d10a6ec9c43125
Zannn210 <fauzan210mohamad@gmail.com>
2026-03-26 00:01:38 +0700

    feat: cmake

---
+++ soal/soal_cmake/build/CMakeFiles/robot.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/asus pc/OneDrive/Documents/URO/Tugas1/SEKURO_TUGAS_1_Fauzan_PROGRAMMING/soal/soal_cmake/main.cpp" "CMakeFiles/robot.dir/main.cpp.obj" "gcc" "CMakeFiles/robot.dir/main.cpp.obj.d"
+  "C:/Users/asus pc/OneDrive/Documents/URO/Tugas1/SEKURO_TUGAS_1_Fauzan_PROGRAMMING/soal/soal_cmake/robot.cpp" "CMakeFiles/robot.dir/robot.cpp.obj" "gcc" "CMakeFiles/robot.dir/robot.cpp.obj.d"
+  "C:/Users/asus pc/OneDrive/Documents/URO/Tugas1/SEKURO_TUGAS_1_Fauzan_PROGRAMMING/soal/soal_cmake/robotair.cpp" "CMakeFiles/robot.dir/robotair.cpp.obj" "gcc" "CMakeFiles/robot.dir/robotair.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ soal/soal_cmake/build/CMakeFiles/robot.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "\$(CMAKE_SOURCE_DIR)/bin/robot.exe"
+  "\$(CMAKE_SOURCE_DIR)/bin/robot.exe.manifest"
+  "\$(CMAKE_SOURCE_DIR)/bin/robot.pdb"
+  "CMakeFiles/robot.dir/main.cpp.obj"
+  "CMakeFiles/robot.dir/main.cpp.obj.d"
+  "CMakeFiles/robot.dir/robot.cpp.obj"
+  "CMakeFiles/robot.dir/robot.cpp.obj.d"
+  "CMakeFiles/robot.dir/robotair.cpp.obj"
+  "CMakeFiles/robot.dir/robotair.cpp.obj.d"
+  "librobot.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/robot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
